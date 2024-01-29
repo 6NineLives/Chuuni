a07354b85531f7963bd328bdf02da23fac9d1e06
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-01-29 12:11:04 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "me.abhigya"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package me.abhigya
+
+fun main() {
+    println("Hello World!")
+}
